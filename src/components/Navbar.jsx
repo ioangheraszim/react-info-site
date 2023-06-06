ffbba9e6ec2b47858e7d7fd3163e0267f69c281c
ioangheraszim <gijohny17@gmail.com>
2023-06-06 20:02:30 +0100

    added some components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export default function Navbar() {
+    return (
+        <>
+        <nav>
+            <img src="../src/asssets/react.svg" alt="react-icon" />
+            <h3></h3>
+            <h4></h4>            
+        </nav>
+        </>
+    )
+}
