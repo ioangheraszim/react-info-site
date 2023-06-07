78972caffdd5d7fc2f0499bf9633f1521e165c54
ioangheraszim <gijohny17@gmail.com>
2023-06-07 22:16:10 +0100

    added some styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from './components/Navbar'
 
 export default function App() {
 
   return (
     <>
-     
+      <Navbar />
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,9 @@ export default function Navbar() {
     return (
         <>
         <nav>
-            <img src="../src/asssets/react.svg" alt="react-icon" />
-            <h3></h3>
-            <h4></h4>            
+            <img className="nav--icon" src="./src/assets/react.svg" alt="react-icon" /> 
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>            
         </nav>
         </>
     )
