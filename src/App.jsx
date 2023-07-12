2ffd618cb3c74420ab542f7336704e3247910744
ioangheraszim <gijohny17@gmail.com>
2023-07-12 22:29:10 +0100

    added toggle for dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
-import Main
- from './components/Main'
+import Main from './components/Main'
+
 export default function App() {
+
+  const [darkMode, setDarkMode] = useState(true)
+
+  const toggleDarkMode = () => {
+    setDarkMode(prevMode => !prevMode)
+  }
 
   return (
     <div className="container">
-            <Navbar />
-            <Main />
+            <Navbar 
+              darkMode = {darkMode} 
+              toggleDarkMode={toggleDarkMode}
+            />
+            <Main darkMode = {darkMode}/>
     </div>
   )
 }
